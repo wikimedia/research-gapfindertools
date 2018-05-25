aa959b5559021a9f6d57dc9c1325d82314c55968
Baha <bmansurov@wikimedia.org>
2018-05-25 16:08:31 -0400

    Add gapfinder-tools.wmflabs.org to allowed hosts

--- gapfindertools/settings.py
+++ gapfindertools/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#)zbti_w!for_jack0xpefbi=&c@tsb2oua4j$e!djyhy&x9g7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gapfinder-tools.wmflabs.org']
 
 
 # Application definition
