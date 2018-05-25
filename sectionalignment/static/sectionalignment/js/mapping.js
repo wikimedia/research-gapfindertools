3989af8ee886dd3f8c29dcc97df22dd81f58015e
Baha <bmansurov@wikimedia.org>
2018-05-25 17:39:35 -0400

    Prevent auto complete from adjusting input width

--- sectionalignment/static/sectionalignment/js/mapping.js
+++ sectionalignment/static/sectionalignment/js/mapping.js
@@ -2,6 +2,7 @@ $(function () {
     function setupAutocomplete($element, suggestions) {
         var options = {
 	        data: suggestions,
+            adjustWidth: false,
             list: {
 		        match: {
 			        enabled: true
@@ -16,7 +17,7 @@ $(function () {
 
     $('body').on('click', '#add-more', function (event) {
         var $newBox = $('<div class="translation-box"></div>')
-            .append('<input type="text" class="translation" name="translation">');
+            .append('<input type="text" class="translation form-control" name="translation">');
 
         $newBox.insertBefore($(event.target));
         setupAutocomplete($newBox.find('input'), window.suggestions);
