e09e1fdb3eb28ac31133cb5d6169d8ccdf3b8d2b
Baha <bmansurov@wikimedia.org>
2018-05-29 16:40:44 -0400

    Admin: allow to download user input
    Also allow to filter by DONE status.

--- sectionalignment/admin.py
+++ sectionalignment/admin.py
@@ -1,4 +1,9 @@
+import csv
+from io import StringIO
+
+
 from django.contrib import admin
+from django.http import HttpResponse
 
 from .models import Mapping, UserInput
 
@@ -6,8 +11,24 @@ admin.site.register(Mapping)
 
 
 class UserInputAdmin(admin.ModelAdmin):
+    actions = ['download_tsv']
+    list_filter = ['done', 'user_session_key']
     search_fields = ['source__title']
-    list_filter = ['user_session_key']
+
+    def download_tsv(self, request, queryset):
+        f = StringIO()
+        writer = csv.writer(f, delimiter='\t')
+        writer.writerow(["source_lang", "source_title", "destination_language",
+                         "destination_title", "user_session_key"])
+        for row in queryset:
+            writer.writerow([row.source.language, row.source.title,
+                             row.destination_language, row.destination_title,
+                             row.user_session_key])
+        f.seek(0)
+        response = HttpResponse(f, content_type='text/tsv')
+        response['Content-Disposition'] = 'attachment; filename=user_input.tsv'
+        return response
+    download_tsv.short_description = "Download selected user inputs"
 
 
 admin.site.register(UserInput, UserInputAdmin)
